0a8084b89a6c4675d461274ab6208c0d8e8f6b0e
Destiny <destinyerhen@gmail.com>
2024-10-17 12:06:28 +0100

    login connected to main window

--- Frontend/main.py
+++ Frontend/main.py
@@ -3,29 +3,17 @@ from PySide6 import QtCore as qtc
 from PySide6 import QtWidgets as qtw
 from PySide6 import QtGui as qtg
 
-from Frontend.Main.ui.MainWindow_ui import Ui_MainWindow
+from Main.ui.MainWindow_ui import Ui_MainWindow
+from login import LoginForm
 
 class MainWindow(qtw.QMainWindow, Ui_MainWindow):
     def __init__(self,):
         super().__init__()
         self.setupUi(self)
 
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    app = qtw.QApplication(sys.argv)
-
-    window = MainWindow()
-    window.show()
-
-    sys.exit(app.exec())
-
+        self.form = LoginForm()
+        self.form.login_success.connect(self.show)
+        self.form.show()
 
 
 
@@ -37,6 +25,8 @@ if __name__ == "__main__":
     app = qtw.QApplication(sys.argv)
 
     window = MainWindow()
-    window.show()
+    
 
     sys.exit(app.exec())
+
+
